3a8803166849377b447fc93a9b1f7e69d87200a4
Salakhov Artur <gsmilexd@gmail.com>
2017-03-30 18:06:42 +0300

    create class for tests

---
+++ test_C_sharp_7/test_C_sharp_7/OutVariablesTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_C_sharp_7
+{
+    static class OutVariablesTests
+    {
+        public static void OutVariablesTest()
+        {
+            string number = Console.ReadLine();
+
+            if (int.TryParse(number, out var num))
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("NaN");
+            }
+        }
+
+        public static void OutVariablesDiscardsTest()
+        {
+            string number = Console.ReadLine();
+
+            if (int.TryParse(number, out _))
+            {
+                Console.WriteLine("Number!");
+            }
+            else
+            {
+                Console.WriteLine("NaN");
+            }
+        }
+    }
+}
--- test_C_sharp_7/test_C_sharp_7/Program.cs
+++ test_C_sharp_7/test_C_sharp_7/Program.cs
@@ -1,43 +1,9 @@
-﻿using System;
-
-namespace test_C_sharp_7
+﻿namespace test_C_sharp_7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            OutVariablesDiscardsTest();
-
-            OutVariablesTest();
         }
-
-        private static void OutVariablesTest()
-        {
-            string number = Console.ReadLine();
-
-            if (int.TryParse(number, out var num))
-            {
-                Console.WriteLine(num);
-            }
-            else
-            {
-                Console.WriteLine("NaN");
-            }
-        }
-
-        private static void OutVariablesDiscardsTest()
-        {
-            string number = Console.ReadLine();
-
-            if (int.TryParse(number, out _))
-            {
-                Console.WriteLine("Number!");
-            }
-            else
-            {
-                Console.WriteLine("NaN");
-            }
-        }
-
-        }
+    }
 }
