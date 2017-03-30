07755d489af7ed4fb091435042053a0b307a2fad
Salakhov Artur <gsmilexd@gmail.com>
2017-03-30 23:35:44 +0300

    tuples tests

--- test_C_sharp_7/test_C_sharp_7/Program.cs
+++ test_C_sharp_7/test_C_sharp_7/Program.cs
@@ -8,11 +8,13 @@
             //PatternMatchingTests.IsExpressionPatternTest("4");
             //PatternMatchingTests.IsExpressionPatternTest(4);
 
-            PatternMatchingTests.SwitchStatementsPatternTest("lala");
-            PatternMatchingTests.SwitchStatementsPatternTest(4);
-            PatternMatchingTests.SwitchStatementsPatternTest(-4);
-            PatternMatchingTests.SwitchStatementsPatternTest(0.3f);
-            PatternMatchingTests.SwitchStatementsPatternTest(null);
+            //PatternMatchingTests.SwitchStatementsPatternTest("lala");
+            //PatternMatchingTests.SwitchStatementsPatternTest(4);
+            //PatternMatchingTests.SwitchStatementsPatternTest(-4);
+            //PatternMatchingTests.SwitchStatementsPatternTest(0.3f);
+            //PatternMatchingTests.SwitchStatementsPatternTest(null);
+
+            TupleTests.ShowName();
         }
     }
 }
---
+++ test_C_sharp_7/test_C_sharp_7/TupleTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test_C_sharp_7
+{
+    static class TupleTests
+    {
+        private static (string firstName, string lastName, string middleName) Name
+        {
+            get
+            {
+                return (firstName: "FirstName", lastName: "LastName", middleName: "MiddleName");
+            }
+        }
+
+        private static (string firstName, string lastName, string middleName) NameWithWrongPosition
+        {
+            get
+            {
+                // "middleName:" and "firstName:" will be ignore
+                return (middleName: "MiddleName", lastName: "LastName", firstName: "FirstName");
+            }
+        }
+
+        public static void ShowName()
+        {
+            var fullName = Name;
+
+            Console.WriteLine($"{ fullName }");
+
+            Console.WriteLine($"{nameof(fullName.lastName)}: {fullName.lastName}");
+            Console.WriteLine($"{nameof(fullName.firstName)}: {fullName.firstName}");
+            Console.WriteLine($"{nameof(fullName.middleName)}: {fullName.middleName}");
+        }
+    }
+}
