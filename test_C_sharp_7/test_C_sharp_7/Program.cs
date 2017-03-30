8951e93fbb6dbf63cef76fb9c6459eb615ac41b3
Salakhov Artur <gsmilexd@gmail.com>
2017-03-30 17:52:23 +0300

    out variables test

---
+++ test_C_sharp_7/test_C_sharp_7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace test_C_sharp_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutVariablesDiscardsTest();
+
+            OutVariablesTest();
+        }
+
+        private static void OutVariablesTest()
+        {
+            string number = Console.ReadLine();
+
+            if (int.TryParse(number, out var num))
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("NaN");
+            }
+        }
+
+        private static void OutVariablesDiscardsTest()
+        {
+            string number = Console.ReadLine();
+
+            if (int.TryParse(number, out _))
+            {
+                Console.WriteLine("Number!");
+            }
+            else
+            {
+                Console.WriteLine("NaN");
+            }
+        }
+
+        }
+}
